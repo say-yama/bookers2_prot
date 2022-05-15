65f43c726f2cbf3b8f2d462df92c2c1eb71e1bfe
EC2 Default User <ec2-user@ip-172-31-47-176.ec2.internal>
2022-05-15 17:03:40 +0000

    [Add]gem devise,model-devise.user.book,controller-homes.user.book,resourceroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  # get 'homes/top'
+  devise_for :users
+  root to: "homes#top"
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :homes
+  resource :books
+  resource :users
 end
